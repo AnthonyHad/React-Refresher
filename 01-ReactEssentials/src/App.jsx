800df5ab6438f2a1da74cea4d9ea4a27c7c05970
Anthony Hadwan <anthony.hadwan@hec.edu>
2024-12-12 23:20:39 +0100

    First components, props and state

--- 01-ReactEssentials/src/App.jsx
+++ 01-ReactEssentials/src/App.jsx
@@ -1,9 +1,9 @@
+import { useState } from "react";
 
-import { CORE_CONCEPTS } from './data.js';
-import Header from './components/Header/Header.jsx';
-import CoreConcept from './components/CoreConcept.jsx';
-import TabButton from './components/TabButton.jsx';
-
+import { CORE_CONCEPTS } from "./data.js";
+import Header from "./components/Header/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 // Props is an object
 
@@ -15,9 +15,18 @@ import TabButton from './components/TabButton.jsx';
 //   </li>
 // }
 
-
-
 function App() {
+  // Hooks are only called inside Component functions
+  // They must not be nested inside code statements
+
+  // Setting state will tell react to reexecute the component
+  const [selectedTopic, setSelectedTopic] = useState("Please click a button");
+
+  function handleClick(selectedTopic) {
+    setSelectedTopic(selectedTopic);
+    console.log(selectedTopic);
+  }
+
   return (
     <div>
       <Header />
@@ -25,16 +34,27 @@ function App() {
         <section id="core-concepts">
           <h2>Core Concepts</h2>
           <ul>
-            <CoreConcept title = {CORE_CONCEPTS[0].title} description={CORE_CONCEPTS[0].description} image={CORE_CONCEPTS[0].image} />
-            < CoreConcept {...CORE_CONCEPTS[1]}  />
-            < CoreConcept {...CORE_CONCEPTS[2]}  />
-            < CoreConcept {...CORE_CONCEPTS[3]}  />
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
           </ul>
         </section>
-        <section id="examples" >
+        <section id="examples">
           <menu>
-          <TabButton>Hola</TabButton>
+            {/* If we don't use arrow function then the function will be called on Render and not on-click */}
+            <TabButton onClick={() => handleClick("components")}>
+              Components
+            </TabButton>
+            <TabButton onClick={() => handleClick("jsx")}>JSX</TabButton>
+            <TabButton onClick={() => handleClick("props")}>Props</TabButton>
+            <TabButton onClick={() => handleClick("state")}>State</TabButton>
           </menu>
+          {selectedTopic}
         </section>
       </main>
     </div>
--- 01-ReactEssentials/src/components/TabButton.jsx
+++ 01-ReactEssentials/src/components/TabButton.jsx
@@ -1,5 +1,9 @@
 //children is what we put between the tags of the component
 
-export default function TabButton({children}) {
-return <li><button>{children}</button></li>
+export default function TabButton({ children, onClick }) {
+  return (
+    <li>
+      <button onClick={onClick}>{children}</button>
+    </li>
+  );
 }
