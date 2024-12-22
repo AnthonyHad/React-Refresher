64d5416cf69b05b1505ab3b84277570f05a67b24
Anthony Hadwan <anthony.hadwan@hec.edu>
2024-12-22 23:35:25 +0300

    new components

--- 01-ReactEssentials/src/App.jsx
+++ 01-ReactEssentials/src/App.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
-
-import { CORE_CONCEPTS } from "./data.js";
 import Header from "./components/Header/Header.jsx";
-import CoreConcept from "./components/CoreConcept.jsx";
-import TabButton from "./components/TabButton.jsx";
-import { EXAMPLES } from "./data.js";
+import CoreConcepts from "./components/CoreConcepts.jsx";
+import Examples from "./components/Examples.jsx";
 
 // Props is an object
 
@@ -20,81 +16,18 @@ function App() {
   // Hooks are only called inside Component functions
   // They must not be nested inside code statements
 
-  // Setting state will tell react to reexecute the component
-  const [selectedTopic, setSelectedTopic] = useState();
-
-  function handleClick(selectedTopic) {
-    setSelectedTopic(selectedTopic);
-    // Even though we are logging the state after updating it. React schedules update and the state is upadted after render.
-    console.log(selectedTopic);
-  }
-
   // We could create a variable called tab content and check if a topic is selected to render content conditionnaly
 
   return (
-    <div>
+    // <div>
+    // Fragments
+    <>
       <Header />
       <main>
-        <section id="core-concepts">
-          <h2>Core Concepts</h2>
-          <ul>
-            {CORE_CONCEPTS.map((conceptItem) => (
-              <CoreConcept key={conceptItem.tite} {...conceptItem} />
-            ))}
-            {/* <CoreConcept
-              title={CORE_CONCEPTS[0].title}
-              description={CORE_CONCEPTS[0].description}
-              image={CORE_CONCEPTS[0].image}
-            />
-            <CoreConcept {...CORE_CONCEPTS[1]} />
-            <CoreConcept {...CORE_CONCEPTS[2]} />
-            <CoreConcept {...CORE_CONCEPTS[3]} /> */}
-          </ul>
-        </section>
-        <section id="examples">
-          <menu>
-            {/* If we don't use arrow function then the function will be called on Render and not on-click */}
-            <TabButton
-              isSelected={selectedTopic === "components"}
-              onClick={() => handleClick("components")}
-            >
-              Components
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "jsx"}
-              onClick={() => handleClick("jsx")}
-            >
-              JSX
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "props"}
-              onClick={() => handleClick("props")}
-            >
-              Props
-            </TabButton>
-            <TabButton
-              isSelected={selectedTopic === "state"}
-              onClick={() => handleClick("state")}
-            >
-              State
-            </TabButton>
-          </menu>
-
-          {!selectedTopic ? <p>Please select a topic</p> : null}
-          {selectedTopic ? (
-            <div id="tab-content">
-              <h3>{EXAMPLES[selectedTopic].title}</h3>
-              <p>{EXAMPLES[selectedTopic].description}</p>
-              <pre>
-                <code>{EXAMPLES[selectedTopic].code}</code>
-              </pre>
-            </div>
-          ) : null}
-
-          {/* We could use the && instead of the terneray experssion which would help us avoid the falsy condition */}
-        </section>
+        <CoreConcepts />
+        <Examples />
       </main>
-    </div>
+    </>
   );
 }
 
---
+++ 01-ReactEssentials/src/components/Examples.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react";
+import TabButton from "./TabButton.jsx";
+
+import { EXAMPLES } from "../data.js";
+
+export default function Examples() {
+  // Setting state will tell react to reexecute the component
+  const [selectedTopic, setSelectedTopic] = useState();
+
+  function handleClick(selectedTopic) {
+    setSelectedTopic(selectedTopic);
+    // Even though we are logging the state after updating it. React schedules update and the state is updated after render.
+    console.log(selectedTopic);
+  }
+  return (
+    <section id="examples">
+      <menu>
+        {/* If we don't use arrow function then the function will be called on Render and not on-click */}
+        <TabButton
+          isSelected={selectedTopic === "components"}
+          onClick={() => handleClick("components")}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "jsx"}
+          onClick={() => handleClick("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "props"}
+          onClick={() => handleClick("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "state"}
+          onClick={() => handleClick("state")}
+        >
+          State
+        </TabButton>
+      </menu>
+
+      {!selectedTopic ? <p>Please select a topic</p> : null}
+      {selectedTopic ? (
+        <div id="tab-content">
+          <h3>{EXAMPLES[selectedTopic].title}</h3>
+          <p>{EXAMPLES[selectedTopic].description}</p>
+          <pre>
+            <code>{EXAMPLES[selectedTopic].code}</code>
+          </pre>
+        </div>
+      ) : null}
+
+      {/* We could use the && instead of the ternery expression which would help us avoid the falsy condition */}
+    </section>
+  );
+}
