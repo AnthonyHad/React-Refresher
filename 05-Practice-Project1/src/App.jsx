7c8019e96a61266e7691c6e5e298d115e9dccdcd
Anthony Hadwan <anthony.hadwan@hec.edu>
2025-01-19 17:42:22 +0100

    Project foundation and styling added

--- 05-Practice-Project1/src/App.jsx
+++ 05-Practice-Project1/src/App.jsx
@@ -1,8 +1,13 @@
+import Sidebar from "./Components/Sidebar";
+import NewProject from "./Components/NewProject";
+import NoProjectSelected from "./Components/NoProjectSelected";
+
 function App() {
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-sreen my-8 flex gap-8">
+      <Sidebar projectName="" />
+      <NoProjectSelected />
+    </main>
   );
 }
 
---
+++ 05-Practice-Project1/src/Components/Input.jsx
@@ -0,0 +1,17 @@
+export default function Input({ textArea, label, ...props }) {
+  const classes =
+    "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
+
+  return (
+    <p className="flex flex-col my-4 gap-1">
+      <label className="text-sm font-bold uppercase text-stone-500">
+        {label}
+      </label>
+      {textArea ? (
+        <textArea className={classes} {...props} />
+      ) : (
+        <input className={classes} {...props} />
+      )}
+    </p>
+  );
+}
