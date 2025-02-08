92eae4e6d52568edaa3d3b11d2e85494692e44da
Anthony Hadwan <anthony.hadwan@hec.edu>
2025-02-08 18:39:31 +0100

    added custom hook

---
+++ 11-CustomHooks/src/hooks/useFetch.js
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react";
+
+// Custom hooks need to start with use and react enforces hook rules.
+
+export function useFetch(fetchFn, initialValue) {
+  const [isFetching, setIsFetching] = useState();
+  const [error, setError] = useState();
+  const [data, setData] = useState(initialValue);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsFetching(true);
+      try {
+        const data = await fetchFn();
+        setData(data);
+      } catch (error) {
+        setError({ message: error.message || "Failed to fetch data" });
+      }
+
+      setIsFetching(false);
+    }
+
+    fetchData();
+  }, [fetchFn]);
+  // You can expose state updating functions as well.
+  return { isFetching, error, data, setData };
+}
