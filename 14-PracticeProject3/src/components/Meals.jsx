4d551a9db6683f497404c04c3303770cc4746e63
Anthony H <anthony.hadwan@hec.edu>
2025-02-15 18:25:14 +0100

    Added meal items component with formatter util

--- 14-PracticeProject3/src/components/Meals.jsx
+++ 14-PracticeProject3/src/components/Meals.jsx
@@ -1,3 +1,27 @@
+import MealItem from "./MealItem";
+
+import { useState, useEffect } from "react";
+
 export default function Meals() {
-    
+  const [loadedMeals, setLoadedMeals] = useState([]);
+
+  useEffect(() => {
+    async function loadMeals() {
+      const response = await fetch("http://localhost:3000/meals");
+
+      const meals = await response.json();
+      setLoadedMeals(meals);
+    }
+
+    loadMeals();
+  }, []);
+
+  console.log(loadedMeals);
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal} />
+      ))}
+    </ul>
+  );
 }
